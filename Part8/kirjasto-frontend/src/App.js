ebc19c2b966fb8906a14da1b026d9bc3cf4d71c0
Niko Hernesniemi <31626530+heniko@users.noreply.github.com>
2021-01-30 15:06:55 +0200

    Add authors view

--- Part8/kirjasto-frontend/src/App.js
+++ Part8/kirjasto-frontend/src/App.js
@@ -1,11 +1,14 @@
-
 import React, { useState } from 'react'
+import { useQuery } from '@apollo/client'
+
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import { ALL_AUTHORS } from './queries'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const authors = useQuery(ALL_AUTHORS)
 
   return (
     <div>
@@ -17,6 +20,7 @@ const App = () => {
 
       <Authors
         show={page === 'authors'}
+        authors={authors}
       />
 
       <Books
--- Part8/kirjasto-frontend/src/components/Authors.js
+++ Part8/kirjasto-frontend/src/components/Authors.js
@@ -1,11 +1,13 @@
-  
 import React from 'react'
 
-const Authors = (props) => {
-  if (!props.show) {
+const Authors = ({ show, authors }) => {
+  if (!show) {
     return null
   }
-  const authors = []
+
+  if (authors.loading) {
+    return null
+  }
 
   return (
     <div>
@@ -21,7 +23,7 @@ const Authors = (props) => {
               books
             </th>
           </tr>
-          {authors.map(a =>
+          {authors.data.allAuthors.map(a =>
             <tr key={a.name}>
               <td>{a.name}</td>
               <td>{a.born}</td>
