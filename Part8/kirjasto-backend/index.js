1176d37ed4842fef0357f7cbaa02182b4997645c
Niko Hernesniemi <31626530+heniko@users.noreply.github.com>
2021-02-10 01:55:46 +0200

    Some fixes

--- Part8/kirjasto-backend/index.js
+++ Part8/kirjasto-backend/index.js
@@ -235,5 +235,5 @@ const server = new ApolloServer({
 
 server.listen().then(({ url, subscriptionsUrl }) => {
   console.log(`Server ready at ${url}`)
-  console.log(`Subscriptions ready ar ${subscriptionsUrl}`)
+  console.log(`Subscriptions ready at ${subscriptionsUrl}`)
 })
