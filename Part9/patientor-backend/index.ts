58d56291833b25de8c85a61ac55a8009b2fc2201
Niko Hernesniemi <31626530+heniko@users.noreply.github.com>
2021-02-14 04:10:14 +0200

    9.9

--- Part9/patientor-backend/index.ts
+++ Part9/patientor-backend/index.ts
@@ -1,14 +1,17 @@
 import express from 'express';
 const app = express();
+
+import cors = require('cors');
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
-  console.log('someone pinged here');
-  res.send('pong');
+app.get('/api/ping', (_req, res) => {
+    console.log('someone pinged here');
+    res.send('pong');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
